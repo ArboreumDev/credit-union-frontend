a9c5b15d720e872cba401af42d52c162f3bca3e4
Paruthi, Gaurav <gparuthi@umich.edu>
2020-07-21 14:55:07 -0700

    Add form

--- pages/lender.tsx
+++ pages/lender.tsx
@@ -1,127 +1,74 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import ButtonAppBar from '../components/AppBar';
-import { InputLabel, Input, FormHelperText, makeStyles, createStyles, Theme, FormControl, Paper, Grid, TextField, Button, Select, MenuItem, FormControlLabel, Radio, RadioGroup, FormLabel, CssBaseline, ThemeProvider, Container } from '@material-ui/core';
-import theme from '../components/theme';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { TextField } from 'formik-material-ui';
+import Button from "@material-ui/core/Button";
+import { LinearProgress, Paper } from '@material-ui/core';
+
+interface Values {
+    email: string;
+    password: string;
+}
 
 export default function NewProject() {
-    return (
-        <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <ButtonAppBar />
-            <Container>
+    return (<div>
+        <Paper>
+            
 
-                <Paper style={{ padding: 16 }}>
-                    <Typography variant="h4" align="left" component="h1" gutterBottom>
-                        Create new project
-      </Typography>
-                    <Grid container alignItems="stretch" spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                required
-                                name="projectName"
-                                type="text"
-                                label="Name"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                name="githubLink"
-                                type="text"
-                                label="Github Link"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                required
-                                name="description"
-                                type="text"
-                                label="Description"
-                                multiline
-                                rows={6}
-                            />
-                        </Grid>
-
-                        <Grid item xs={12}>
-                            <FormControl>
-                                <FormLabel>Project Type</FormLabel>
-                                <RadioGroup
-                                    style={{ flexDirection: "row" }}
-                                >
-                                    <FormControlLabel
-                                        control={<Radio />}
-                                        label="coding"
-                                        value="code"
-                                    />
-                                    <FormControlLabel
-                                        control={<Radio />}
-                                        label="qualitative"
-                                        value="qual"
-                                    />
-                                    <FormControlLabel
-                                        control={<Radio />}
-                                        label="quantitative"
-                                        value="quant"
-                                    />
-                                </RadioGroup>
-                            </FormControl>
-                        </Grid>
-
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                required
-                                name="incentive"
-                                type="text"
-                                label="Incentive"
-                                multiline
-                                rows={2}
-                            />
-                        </Grid>
-
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                required
-                                name="process"
-                                type="text"
-                                label="Cadidate Selection Process"
-                                multiline
-                                rows={4}
-                            />
-                        </Grid>
-
-                        <Grid item xs={12}>
-                            <TextField
-                                fullWidth
-                                required
-                                name="geography"
-                                type="text"
-                                label="Geography"
-
-                            />
-                        </Grid>
-
-
-                        <Grid item style={{ marginTop: 16 }}>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                type="submit"
-                            // disabled={submitting}
-                            >
-                                Submit
-                  </Button>
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Container>
-        </ThemeProvider>
-
+         
+        <Formik
+            initialValues={{
+                email: '',
+                password: '',
+            }}
+            validate={values => {
+                const errors: Partial<Values> = {};
+                if (!values.email) {
+                    errors.email = 'Required';
+                } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+                ) {
+                    errors.email = 'Invalid email address';
+                }
+                return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    setSubmitting(false);
+                    alert(JSON.stringify(values, null, 2));
+                }, 500);
+            }}
+        >
+            {({ submitForm, isSubmitting }) => (
+                <Form>
+                    <Field
+                        component={TextField}
+                        name="email"
+                        type="email"
+                        label="Email"
+                    />
+                    <br />
+                    <Field
+                        component={TextField}
+                        type="password"
+                        label="Password"
+                        name="password"
+                    />
+                    {isSubmitting && <LinearProgress />}
+                    <br />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        disabled={isSubmitting}
+                        onClick={submitForm}
+                    >
+                        Submit
+          </Button>
+                </Form>
+            )}
+        </Formik>
+        </Paper>
+             
+            </div>
 
     );
 }
