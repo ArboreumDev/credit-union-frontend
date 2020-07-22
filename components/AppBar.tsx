b4baf663cc2d10ba486067ce91b4b1af3d2ad951
Paruthi, Gaurav <gparuthi@umich.edu>
2020-07-22 10:53:58 -0700

    App bar login

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,6 +7,9 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Link from 'next/link';
+import GoogleLogin, { GoogleLogout } from 'react-google-login';
+import { useStore } from '../stores/root';
+import { useSession } from 'next-auth/client'
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -26,18 +29,27 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function ButtonAppBar() {
     const classes = useStyles();
+    const [session, loading] = useSession()
 
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton> */}
-                    
-                    <Link href='/'><Typography variant="h6" className={classes.title}>Vicara</Typography></Link> 
-                    <Button href='/project/new' color="inherit">Create</Button>
-                    <Button href='/project/search' color="inherit">Search</Button>
+
+                    <Link href='/'><Typography variant="h6" className={classes.title}>Vicara</Typography></Link>
+                    {!session && <>
+                        Not signed in <br />
+                        <Button href='/project/new' color="inherit">Create</Button>
+                        <a href="/api/auth/signin">Sign in</a>
+                    </>}
+                    {session && <>
+                        Signed in as {session.user.email} <br />
+                        <a href="/api/auth/signout">Sign out</a>
+                        <Button href='/project/search' color="inherit">Search</Button>
+                    </>}
+
+
+
                 </Toolbar>
             </AppBar>
         </div>
