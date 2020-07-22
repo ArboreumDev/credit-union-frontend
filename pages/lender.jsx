9480da3fca2923edb278487d847916bd9770b63a
Paruthi, Gaurav <gparuthi@umich.edu>
2020-07-22 11:22:16 -0700

    Update create lender

---
+++ pages/lender.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ButtonAppBar from '../components/AppBar';
+import { useStore } from '../stores/root';
+import { observer } from 'mobx-react-lite';
+
+// import GoogleLogin, { GoogleLogout } from '../dist/google-login'
+// import FontAwesome from 'react-fontawesome';
+
+
+export default observer(() => {
+    const store = useStore()
+
+    return <div>
+        <ButtonAppBar />
+        Hi {store.session.user.name} {store.session.user.email}
+        <br />
+        <br />
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeI7HkDCLcfS4XYAHgOeUAsA2pRqU4evlgSMOxFEwJikOP-CA/viewform?embedded=true" width="640" height="642" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </div>
+})
