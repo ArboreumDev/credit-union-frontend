e2a566e312839b2a35aa031e0115254e011a3aa2
Paruthi, Gaurav <gparuthi@umich.edu>
2020-07-22 14:23:15 -0700

    Config

---
+++ components/TrustedUsersTable.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { BorrowerInstance } from '../stores/root';
+
+const useStyles = makeStyles({
+    table: {
+        // minWidth: 650,
+    },
+});
+
+interface Props {
+    rows: BorrowerInstance[]
+}
+
+export default function SimpleTable(props: Props) {
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Namer</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Amount</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.rows.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell>{row.email}</TableCell>
+                            <TableCell>{row.amount}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
--- pages/lender.jsx
+++ pages/lender.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ButtonAppBar from '../components/AppBar';
+import UserConfig from '../components/UserConfig';
 import { useStore } from '../stores/root';
 import { observer } from 'mobx-react-lite';
+import { Paper, Typography } from '@material-ui/core';
 
 // import GoogleLogin, { GoogleLogout } from '../dist/google-login'
 // import FontAwesome from 'react-fontawesome';
@@ -12,12 +14,18 @@ export default observer(() => {
 
     return <div>
         <ButtonAppBar />
+
+        <Paper variant="outlined" square >
         {store.session && <>
-            Hi {store.session.user.name} {store.session.user.email}
+
+                {/* <Typography> Hi {store.session.user.name} {store.session.user.email}</Typography> */}
+                <UserConfig/>
             <br />
             <br />
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeI7HkDCLcfS4XYAHgOeUAsA2pRqU4evlgSMOxFEwJikOP-CA/viewform?embedded=true" width="640" height="642" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            
+            
+            {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeI7HkDCLcfS4XYAHgOeUAsA2pRqU4evlgSMOxFEwJikOP-CA/viewform?embedded=true" width="640" height="642" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> */}
         </>}
-        
+        </Paper>
     </div>
 })
