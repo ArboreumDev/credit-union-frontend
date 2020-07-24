a795ecfdd12c777a0157d9167fff95f229b92d2e
Paruthi, Gaurav <gparuthi@umich.edu>
2020-07-24 13:15:09 -0700

    Add video

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ import Link from 'next/link';
 import useSWR from 'swr'
 import { fetcher } from '../utils/api';
 import { Classes } from '@blueprintjs/core';
+import YoutubeBackground from 'react-youtube-background'
 
+import ReactPlayer from 'react-player'
 
 export default function Home() {
 
@@ -15,11 +17,37 @@ export default function Home() {
   // if (!data) return <div>Loading...</div>
 
   // const { users } = data
-
+  
   return (
-    
-          <Link href='/lender'><a>Lender Config</a></Link> 
-    
+    <div className='Container'>
+        <video autoPlay={true} loop={true} muted className='Video' >
+          <source src={"/videos/network2.mp4"} type="video/mp4" />
+                Your browser does not support the video tag.
+        </video>
+        
+
+      <style jsx>{`
+      .Container {
+    position: relative;
+    min-height: 300px;
+    max-height: 100%;
+    overflow: hidden;
+    text-align: center;
+
+                margin: auto;
+                width: 640;
+                padding: 10px;
+            }
+       .Video {
+    width: 100%;
+    height: 100%;
+  }
+            `
+      }
+      </style>
+
+    </div>
+          
     
 )
 }
