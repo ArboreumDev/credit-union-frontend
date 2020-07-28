a3722398deaba363f3369ba8a48c05cc74f610ce
Paruthi, Gaurav <gparuthi@umich.edu>
2020-07-27 18:30:28 -0700

    remove email auth for now

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,16 +8,16 @@ const options = {
   // Configure one or more authentication providers
   providers: [
     Providers.Email({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: encodeURIComponent(process.env.EMAIL_SERVER_PASSWORD),
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    }),
+    //   server: {
+    //     host: process.env.EMAIL_SERVER_HOST,
+    //     port: process.env.EMAIL_SERVER_PORT,
+    //     auth: {
+    //       user: process.env.EMAIL_SERVER_USER,
+    //       pass: encodeURIComponent(process.env.EMAIL_SERVER_PASSWORD),
+    //     },
+    //   },
+    //   from: process.env.EMAIL_FROM,
+    // }),
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
