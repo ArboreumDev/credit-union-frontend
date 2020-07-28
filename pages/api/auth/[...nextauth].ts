1c452671a16245056737220a109f8ae95bd183ca
Paruthi, Gaurav <gparuthi@umich.edu>
2020-07-27 18:00:22 -0700

    add pg back

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,41 +3,51 @@ import Providers from 'next-auth/providers'
 
 
 const options = {
-    site: process.env.SITE || 'http://mywebsite.com:3000',
+  site: process.env.SITE || "http://mywebsite.com:3000",
 
-    // Configure one or more authentication providers
-    providers: [
-        // Providers.GitHub({
-        //     clientId: process.env.GITHUB_ID,
-        //     clientSecret: process.env.GITHUB_SECRET
-        // }),
-        Providers.Google({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
-    ],
+  // Configure one or more authentication providers
+  providers: [
+    Providers.Email({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 
-    // A database is optional, but required to persist accounts in a database
-    database: process.env.DATABASE_URL,
-    callbacks: {
-        // signin: async (profile, account, metadata) => { console.log(profile, account, metadata)},
-        // redirect: async (url, baseUrl) => {
-        //     console.log(url, baseUrl)
-        //     url.startsWith(baseUrl)
-        //         ? Promise.resolve(url)
-        //         : Promise.resolve(baseUrl)
-        //  },
-        
-        // jwt: async (token) => { }
+  // A database is optional, but required to persist accounts in a database
+  database: process.env.DATABASE_URL,
+  callbacks: {
+    // signin: async (profile, account, metadata) => { console.log(profile, account, metadata)},
+    // redirect: async (url, baseUrl) => {
+    //     console.log(url, baseUrl)
+    //     url.startsWith(baseUrl)
+    //         ? Promise.resolve(url)
+    //         : Promise.resolve(baseUrl)
+    //  },
+    // jwt: async (token) => { }
+  },
+  events: {
+    signin: async (message) => {},
+    signout: async (message) => {
+      console.log("sign out successful");
     },
-    events: {
-        signin: async(message) => {},
-        signout: async(message) => { console.log('sign out successful') },
-        createUser: async(message) => { /* user created */ },
-        linkAccount: async(message) => { /* account linked to a user */ },
-        session: async(message) => { /* session is active */ },
-        error: async(message) => { /* error in authentication flow */ }
-    }
-}
+    createUser: async (message) => {
+      /* user created */
+    },
+    linkAccount: async (message) => {
+      /* account linked to a user */
+    },
+    session: async (message) => {
+      /* session is active */
+    },
+    error: async (message) => {
+      /* error in authentication flow */
+    },
+  },
+};
 
 export default (req, res) => NextAuth(req, res, options)
