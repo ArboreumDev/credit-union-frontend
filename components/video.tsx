53b94df5d17cf9d51ca623f52c2c2b1196411b83
Paruthi, Gaurav <gparuthi@umich.edu>
2020-07-27 18:17:42 -0700

    vid source to aws

--- components/video.tsx
+++ components/video.tsx
@@ -1,29 +1,32 @@
 export default function Video() {
-    return <div className='Container'>
-        <video autoPlay={true} loop={true} muted className='Video' >
-            <source src={"/videos/network2.mp4"} type="video/mp4" />
-                Your browser does not support the video tag.
+    return (
+      <div className="Container">
+        <video autoPlay={true} loop={true} muted className="Video">
+          <source
+            src={"https://arboreum.s3.eu-west-2.amazonaws.com/network.mov"}
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
         </video>
 
+        <style jsx>
+          {`
+            .Container {
+              position: relative;
+              min-height: 300px;
+              max-height: 100%;
+              overflow: hidden;
+              text-align: center;
 
-        <style jsx>{`
-      .Container {
-    position: relative;
-    min-height: 300px;
-    max-height: 100%;
-    overflow: hidden;
-    text-align: center;
-
-                margin: auto;
-                width: 640;
-                padding: 10px;
+              margin: auto;
+              width: 640;
+              padding: 10px;
             }
-       .Video {
-    width: 100%;
-    height: 100%;
-  }
-            `
-        }
+            .Video {
+              width: 100%;
+              height: 100%;
+            }
+          `}
         </style>
-
-    </div>}
+      </div>
+    );}
