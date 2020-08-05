e6b658bc9aeee3ab23a7143554f5be9f29f07a2d
Paruthi, Gaurav <gparuthi@umich.edu>
2020-08-05 15:41:47 -0700

    Add mutation

--- pages/onboarding.tsx
+++ pages/onboarding.tsx
@@ -1,12 +1,39 @@
 import Link from "next/link";
 import { useSession } from "next-auth/client"
-import { SessionUser } from "../utils/interfaces";
+import { SessionUser, User } from "../utils/types";
 import { Card, H4, Button, H5, NumericInput, InputGroup, FormGroup, H1, Checkbox, Radio } from "@blueprintjs/core";
 import Axios from "axios";
+import { initializeGQL } from "../utils/graphql_client";
+import { useRouter } from "next/dist/client/router";
 
+
+const CREATE_USER_MUTATION = /* GraphQL */ `
+  mutation CreateUser(
+    $name: String!
+    $email: String!
+    $user_type: user_t!
+    $phone: String!
+  ) {
+    insert_user_one(
+      object: {
+        email: $email
+        user_type: $user_type
+        name: $name
+        phone: $phone
+      }
+    ) {
+      id
+      created_at
+      email
+    }
+  }
+`
 
 export default function Onboarding() {
+  const router = useRouter()
   const [session, loading] = useSession()
+  const gqlClient = initializeGQL()
+
   if (loading) return <div>Loading...</div>
   const user = session.user as SessionUser
 
@@ -14,11 +41,15 @@ export default function Onboarding() {
     event.preventDefault()
 
     const formData = new FormData(event.target)
-    const data = Object.fromEntries(formData.entries())
-    console.log(data)   
-    Axios
-      .post('/api/new_user', {data: data})
-      .then((res)=> console.log("Done", res.data))
+    const formDataObject = Object.fromEntries(formData.entries())
+    const variables = { ...formDataObject, name: user.name, email: user.email } as User
+
+    // Call mutation
+    gqlClient.request(CREATE_USER_MUTATION, variables).then((res) => {
+      console.log(res)
+      // return to home
+      router.push("/")
+    })
   }
 
   return (
@@ -32,8 +63,8 @@ export default function Onboarding() {
           </FormGroup>
           <div>
             <H4>Do you plan to lend or borrow?</H4>
-            <Radio name="userType" value="Lender" label="Lend" inline checked />
-            <Radio name="userType" value="Borrower" label="Borrow" inline />
+            <Radio name="user_type" value="lender" label="Lend" inline defaultChecked />
+            <Radio name="user_type" value="borrower" label="Borrow" inline />
           </div>
           <Button type="submit" intent="primary">
             Submit
