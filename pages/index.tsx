d8100b3f71fc2db1443fef29075e3f66a5bef911
Paruthi, Gaurav <gparuthi@umich.edu>
2020-08-05 15:41:55 -0700

    Add user query

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,65 +3,45 @@ import useSWR from 'swr'
 import { fetcher } from '../utils/api';
 import { Classes } from '@blueprintjs/core';
 
-import { getSession } from 'next-auth/client'
+import { getSession, useSession } from 'next-auth/client'
 import {AppBarSignedIn, AppBarSignedOut} from '../components/AppBar';
 import Dashboard from '../components/dashboard/dashboard';
 import dynamic from "next/dynamic";
 import Video from '../components/video';
-import { User, UserType } from '../utils/interfaces';
+import { User, UserType, Session } from '../utils/types';
 import { useRouter } from 'next/dist/client/router';
 import Onboarding from './onboarding';
+import { initializeGQL } from '../utils/graphql_client';
+import { useEffect } from 'react';
+
+
 
 const Page = (props: {session, user?: User}) => {
   const router = useRouter();
-  if (!props.session) return <div><AppBarSignedOut/><Video /></div>
-  else {
-    
-      if (!props.user) {
-        // if Onboarding
-        return <Onboarding />
-      } else {
-        // if user is lender, show lender dashboard
-        if (props.user.type == UserType.Lender) router.push("/lender");
-        // if user is borrower, show borrower dashboard
-        if (props.user.type == UserType.Borrower) router.push("/borrower");
-        
-      }
-      return <div></div>
+  
+  useEffect(() => {
+    console.log(props, router, props.user.user_type == UserType.Lender)
+    if (!props.user) {
+      router.push("/onboarding")
+    } else {
+      // if user is lender, show lender dashboard
+      if (props.user.user_type == UserType.Lender) router.push("/lender")
+      // if user is borrower, show borrower dashboard
+      if (props.user.user_type == UserType.Borrower) router.push("/borrower")
     }
+  })
+
+  return <div><AppBarSignedOut/><Video /></div>
 }
 
 Page.getInitialProps = async (context) => {
+  const gqlClient = initializeGQL()
+  const session = await getSession(context) as Session
+  const data = await gqlClient.request(GET_USER_BY_EMAIL, {email: session.user.email})
   return {
-    session: await getSession(context),
-    // TOOD: Fetch user here
-
+    session: session,
+    user: data.user[0]
   }
 }
 
 export default Page
-
-// export default function Home() {
-//   const [session, loading] = useSession()
-
-
-//   // const { data, error } = useSWR('{ users { name } }', fetcher)
-
-//   // if (error) return <div>Failed to load</div>
-//   // if (!data) return <div>Loading...</div>
-
-//   // const { users } = data
-  
-//   return (
-    // <div>
-    //   {!session && <>
-    //     <Video/>
-    //   </>}
-    //   {session && <>
-    //     <Dashboard />
-    //   </>
-    //   }
-    // </div>
-    
-// )
-// }
