9a227a7c9a3b357e50caca91bc3b237a2914b42d
Paruthi, Gaurav <1958947+gparuthi@users.noreply.github.com>
2020-08-10 11:44:13 -0700

    Add react hooks form library to handle forms (#27)
    * Add form
    
    * Fix form

--- pages/onboarding.tsx
+++ pages/onboarding.tsx
@@ -1,8 +1,9 @@
-import Link from "next/link";
 import { useSession } from "next-auth/client"
 import { User } from "../utils/types";
+
+import { useForm } from "react-hook-form"
 import { Card, H4, Button, H5, NumericInput, InputGroup, FormGroup, H1, Checkbox, Radio } from "@blueprintjs/core";
-import Axios from "axios";
+
 import { initializeGQL } from "../utils/graphql_client";
 import { useRouter } from "next/dist/client/router";
 
@@ -29,7 +30,13 @@ const CREATE_USER_MUTATION = /* GraphQL */ `
   }
 `
 
+type FormData = {
+  phone: string;
+  user_type: string
+};
+
 export default function Onboarding() {
+  const { register, setValue, handleSubmit, errors } = useForm<FormData>()
   const router = useRouter()
   const [session, loading] = useSession()
   const gqlClient = initializeGQL()
@@ -37,12 +44,8 @@ export default function Onboarding() {
   if (loading) return <div>Loading...</div>
   const user = session.user as User
 
-  const handleSubmit = (event)=>{
-    event.preventDefault()
-
-    const formData = new FormData(event.target)
-    const formDataObject = Object.fromEntries(formData.entries())
-    const variables = { ...formDataObject, name: user.name, email: user.email } as User
+  const onSubmit = (data)=>{
+    const variables = { ...data, name: user.name, email: user.email } as User
 
     // Call mutation
     gqlClient.request(CREATE_USER_MUTATION, variables).then((res) => {
@@ -57,14 +60,14 @@ export default function Onboarding() {
       <H1>Sign up</H1>
       <Card className="profile-card">
         <H4>Hi {user.name}</H4>
-        <form onSubmit={handleSubmit} method="post">
+        <form onSubmit={handleSubmit(onSubmit)} method="post">
           <FormGroup label="Please enter your phone" labelFor="text-input">
-            <InputGroup name="phone" />
+            <InputGroup name="phone" inputRef={register({ required: true })} />
           </FormGroup>
           <div>
             <H4>Do you plan to lend or borrow?</H4>
-            <Radio name="user_type" value="lender" label="Lend" inline defaultChecked />
-            <Radio name="user_type" value="borrower" label="Borrow" inline />
+            <Radio name="user_type" value="lender" label="Lend" inline defaultChecked inputRef={register({ required: true })} />
+            <Radio name="user_type" value="borrower" label="Borrow" inline inputRef={register({ required: true })} />
           </div>
           <Button type="submit" intent="primary">
             Submit
