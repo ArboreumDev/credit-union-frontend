d16e23a786f1d31c02d96c121531e2ef635add27
Paruthi, Gaurav <gparuthi@umich.edu>
2020-08-24 14:28:47 -0700

    Add simple button

--- src/pages/frontpage.tsx
+++ src/pages/frontpage.tsx
@@ -1,14 +1,38 @@
 import axios from "axios"
 import { Fragment } from "react"
+import {
+  Button,
+  ButtonGroup,
+  Stack,
+  VStack,
+  StackDivider,
+  Box,
+} from "@chakra-ui/core"
+import { FiLogIn } from "react-icons/fi"
+import Router from "next/router"
 
 const FrontPage = () => {
   return (
     <div className="Container">
-      <div>
-        <div>
+      <VStack
+        divider={<StackDivider borderColor="gray.200" />}
+        spacing={4}
+        align="stretch"
+      >
+        <Box h="40px">
           <img width="150px" src="/images/logo.svg" alt="logo" />
-        </div>
-      </div>
+        </Box>
+        <Box h="40px">
+          <Button
+            onClick={() => Router.push("/api/auth/signin")}
+            rightIcon={<FiLogIn />}
+            colorScheme="blue"
+            variant="outline"
+          >
+            Login
+          </Button>
+        </Box>
+      </VStack>
 
       <style jsx>
         {`
