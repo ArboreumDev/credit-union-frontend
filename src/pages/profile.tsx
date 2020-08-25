bd5fe468d2b75b46decce91d12c48ce2830b1e23
Paruthi, Gaurav <gparuthi@umich.edu>
2020-08-25 16:42:45 -0700

    Fix profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -3,17 +3,27 @@ import { getSessionAsProps } from "../utils/ssr"
 import AppBar from "../components/AppBar"
 import { CgLogOut } from "react-icons/cg"
 import { Button } from "@chakra-ui/core"
+import Router from "next/router"
+import { getSession } from "next-auth/client"
 
 const Page = (props: { session: Session }) => {
   return (
     <div>
       <AppBar />
-      <Button rightIcon={<CgLogOut />} colorScheme="blue" variant="outline">
+      <Button
+        onClick={() => Router.push("/api/auth/signout")}
+        rightIcon={<CgLogOut />}
+        colorScheme="blue"
+        variant="outline"
+      >
         Logout
       </Button>
     </div>
   )
 }
-Page.getInitialProps = (context) => getSessionAsProps(context)
+Page.getInitialProps = async (context) => {
+  const session = (await getSession(context)) as Session
+  return { session: session }
+}
 
 export default Page
