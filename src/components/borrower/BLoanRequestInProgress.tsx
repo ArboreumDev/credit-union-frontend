392f1677d914733aa9539bbbc212bb5f993fa99c
Paruthi, Gaurav <gparuthi@umich.edu>
2020-08-28 22:17:19 -0700

    Show loan request details

--- src/components/borrower/BLoanRequestInProgress.tsx
+++ src/components/borrower/BLoanRequestInProgress.tsx
@@ -1,6 +1,61 @@
 import KYCCompleted from "./Notifications/KYCCompleted"
-import { Center, Button } from "@chakra-ui/core"
+import {
+  Container,
+  Stack,
+  Center,
+  Button,
+  Spinner,
+  Box,
+  Text,
+  ListIcon,
+  ListItem,
+  List,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from "@chakra-ui/core"
+import { useSession } from "next-auth/client"
+import { Session, LoanRequestStatus } from "../../utils/types"
+import { bool } from "aws-sdk/clients/signer"
+import { CgFileDocument } from "react-icons/cg"
+import { FaCheckCircle } from "react-icons/fa"
 
 export default function BLoanRequestInProgress() {
-  return <div>Loan Request in Progress!</div>
+  const [session, loading]: [Session, bool] = useSession()
+  if (loading) return <Spinner />
+
+  const user = session.user
+  console.log(user)
+  const loanRequest = session.user.loan_requests.sort(
+    (l1, l2) => Date.parse(l2.created_at) - Date.parse(l1.created_at)
+  )[0]
+  return (
+    <Container maxW="400px" bg="white">
+      <Stack spacing={1}>
+        <Center>
+          <Text padding="0px" margin="0px" fontSize="100px">
+            <CgFileDocument />
+          </Text>
+        </Center>
+        <Center>
+          <Text>Your loan request is being processed.</Text>
+        </Center>
+        <Box>
+          <StatGroup>
+            <Stat>
+              <StatLabel>Amount</StatLabel>
+              <StatNumber>INR {loanRequest.amount}</StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Purpose</StatLabel>
+              <StatNumber>{loanRequest.purpose}</StatNumber>
+            </Stat>
+          </StatGroup>
+        </Box>
+      </Stack>
+    </Container>
+  )
 }
