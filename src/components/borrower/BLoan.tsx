6ac6daa1f070d8506d2f36397115f77d0fb910e4
Paruthi, Gaurav <gparuthi@umich.edu>
2020-08-31 16:08:52 -0700

    Loan UX

--- src/components/borrower/BLoan.tsx
+++ src/components/borrower/BLoan.tsx
@@ -7,27 +7,33 @@ import {
   StatLabel,
   StatNumber,
   Text,
+  Button,
 } from "@chakra-ui/core"
 import { CgFileDocument } from "react-icons/cg"
 import { LoanRequest } from "../../utils/types"
-import { Details } from "./common/Details"
+import { Details, Row } from "./common/Details"
 
 interface Params {
   loan: LoanRequest
 }
 
-const getTableObjectFromLoanRequest = (loan: LoanRequest) =>
-  Object.entries({
-    Status: "To Be disbursed",
-    "Loan Amount": "INR 1,20,000",
-    Repaid: "INR 6000",
-    "Outstanding Principal": "INR 1,20,000",
-    "Outstanding Interest": "INR 20,000",
-    "Last Repayment Date": "30 Septembed 2020",
-  })
+const getTableObjectFromLoanRequest = (loan: LoanRequest): Row[] => [
+  { key: "Status", value: "To Be disbursed" },
+  { key: "Loan Amount", value: "INR 1,20,000" },
+  { key: "Repaid", value: "INR 6000" },
+  { key: "Outstanding Principal", value: "INR 1,20,000" },
+  { key: "Outstanding Interest", value: "INR 20,000" },
+  { key: "Last Repayment Date", value: "30 August 2020" },
+  { key: "Next Repayment Amount", value: "INR 6,000" },
+  {
+    key: "Next Repayment Due Date",
+    value: "30 Septembed 2020",
+    color: "red.500",
+  },
+]
 
 export const BOngoingLoan = ({ loan: loanRequest }: Params) => (
-  <Container minW="s" bg="white">
+  <Container minW="sm" bg="white">
     <Box margin="20px" padding="10px" borderWidth="5px" borderRadius="lg">
       <Center>
         <Text padding="0px" margin="0px" fontSize="30px">
@@ -49,7 +55,11 @@ export const BOngoingLoan = ({ loan: loanRequest }: Params) => (
         </Stat>
       </StatGroup>
       <Box h="30px" />
-      <Details keyValueTuples={getTableObjectFromLoanRequest(loanRequest)} />
+      <Details rows={getTableObjectFromLoanRequest(loanRequest)} />
+      <Box h="30px" />
+      <Center>
+        <Button>Make Repayment</Button>
+      </Center>
     </Box>
   </Container>
 )
--- src/components/borrower/common/Details.tsx
+++ src/components/borrower/common/Details.tsx
@@ -1,20 +1,26 @@
 import { Box, Center, Flex, Text, Stack } from "@chakra-ui/core"
 
-interface Params {
-  keyValueTuples: [string, string][]
+export type Row = {
+  key: string
+  value: string
+  color?: string
 }
 
-export const Details = ({ keyValueTuples }: Params) => (
+interface Params {
+  rows: Row[]
+}
+
+export const Details = ({ rows }: Params) => (
   <Center>
     <Stack>
-      {keyValueTuples.map((tup) => (
-        <Flex key={tup[0]}>
+      {rows.map((row) => (
+        <Flex key={row.key}>
           <Box w="200px">
-            <Text color="gray.500">{tup[0]}</Text>
+            <Text color="gray.500">{row.key}</Text>
           </Box>
           <Box flex="1">
-            <Text color="red.500" align="right">
-              {tup[1]}
+            <Text color={row.color || "black"} align="right">
+              {row.value}
             </Text>
           </Box>
         </Flex>
