07462b3ccbf23a3d4d218739626e90f58d196380
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-01 22:26:56 -0700

    Remove unn imports

--- src/components/borrower/BLoanDashboard.tsx
+++ src/components/borrower/BLoanDashboard.tsx
@@ -1,5 +1,3 @@
-import KYCCompleted from "./Notifications/KYCCompleted"
-import { Center, Button } from "@chakra-ui/core"
 import Dashboard from "../dashboard/borrower"
 
 export default function BLoanDashboard() {
