7dfbc46292ffa04f6d97b93df47315a36512cdd4
djudjuu <julfaber@gmail.com>
2020-09-02 20:04:32 +0200

    create types to match swarmAi input

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -30,6 +30,44 @@ export type JWTToken = {
   user?: User
 }
 
+// =========== the following types mirror the types expected by the swarmAI module ========
+export type SupporterInfo = {
+  supporter_id: string
+  trust_amount: number
+  recommendation_risk: RiskParams
+}
+
+// this type mirrors the type expected by the swarmAI module
+export type BorrowerInfo = {
+  borrower_id: string
+  education_years: number
+  income: number
+  credit_score: number
+}
+
+export type LiveLoanInfo = {
+  loan_id: string
+  interest: number
+  kumr_params: KumrParams
+  amount_owned_portfolio: number
+  amount_owned_supporters: number
+  time_remaining: number
+  loan_schedule: any
+}
+
+export type RiskInput = {
+  central_risk_info: RiskParams
+}
+
+export type OptimizerContext = {
+  risk_free_interest_rate?: number
+  supporter_corpus_share: number
+  loans_in_corpus: LiveLoanInfo[]
+  corpus_cash: number
+  novation: boolean
+}
+// =========== End of risk module types ========
+
 export enum EDGE_STATUS {
   active = "active",
   awaiting_lender_confirmation = "awaiting_lender_confirmation",
