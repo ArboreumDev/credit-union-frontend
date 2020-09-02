0ce641c3094902e754f762d78c90940f14d154c9
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-01 22:27:23 -0700

    Make notif layout consistent

--- src/components/borrower/Notifications/KYCCompleted.tsx
+++ src/components/borrower/Notifications/KYCCompleted.tsx
@@ -2,29 +2,27 @@ import { Alert, AlertIcon, AlertTitle, AlertDescription } from "@chakra-ui/core"
 
 export default function KYCCompleted() {
   return (
-    <div>
-      <Alert
-        status="success"
-        variant="subtle"
-        flexDirection="column"
-        justifyContent="center"
-        textAlign="center"
-        height="200px"
-        width="inherit"
-        marginTop="20px"
-        marginBottom="20px"
-      >
-        <AlertIcon boxSize="40px" mr={0} />
-        <AlertTitle mt={4} mb={1} fontSize="lg">
-          Congrats!
-        </AlertTitle>
-        <AlertDescription maxWidth="sm">
-          KYC has been completed successfully
-        </AlertDescription>
-        <AlertDescription maxWidth="sm">
-          You can now request a loan
-        </AlertDescription>
-      </Alert>
-    </div>
+    <Alert
+      status="success"
+      variant="subtle"
+      flexDirection="column"
+      justifyContent="center"
+      textAlign="center"
+      height="200px"
+      width="inherit"
+      marginTop="20px"
+      marginBottom="20px"
+    >
+      <AlertIcon boxSize="40px" mr={0} />
+      <AlertTitle mt={4} mb={1} fontSize="lg">
+        Congrats!
+      </AlertTitle>
+      <AlertDescription maxWidth="sm">
+        KYC has been completed successfully
+      </AlertDescription>
+      <AlertDescription maxWidth="sm">
+        You can now request a loan
+      </AlertDescription>
+    </Alert>
   )
 }
