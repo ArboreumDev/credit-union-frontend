f10dd69d048f10a5600d871894abcc6d92323e25
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-06 19:50:57 -0700

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import * as React from "react"
 import { Provider } from "next-auth/client"
 import { AppProps } from "next/app"
+import Head from "next/head"
 
 import { ChakraProvider } from "@chakra-ui/core"
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Provider>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id={process.env.UMAMI_WEBSITE_ID}
+          src="https://analytics-umami.vercel.app/umami.js"
+        ></script>
+      </Head>
       <ChakraProvider resetCSS>
         <div>
           <div className="content">
