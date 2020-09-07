edd862fcf075d85199a03a85407c7203924cd5e3
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-07 13:24:57 -0700

    New Loan UX

--- src/components/borrower/BReadyToMakeNewLoan.tsx
+++ src/components/borrower/BReadyToMakeNewLoan.tsx
@@ -1,7 +1,8 @@
 import KYCCompleted from "./Notifications/KYCCompleted"
-import { Center, Button } from "@chakra-ui/core"
+import { Center, Button, Heading, Box } from "@chakra-ui/core"
 import CreateLoanModal from "./CreateLoan/CreateLoanModal"
 import { User } from "../../utils/types"
+import CreateLoanForm from "./CreateLoan/CreateLoanForm"
 
 interface Props {
   user: User
@@ -9,11 +10,16 @@ interface Props {
 
 export default function BReadyToMakeNewLoan({ user }: Props) {
   return (
-    <div>
+    <Box>
       <KYCCompleted />
-      <Center height="20px">
-        <CreateLoanModal user={user} />
+      <Box h="30px" />
+      <Center>
+        <Heading as="h1" size="lg">
+          Request Loan
+        </Heading>
       </Center>
-    </div>
+      <Box h="30px" />
+      <CreateLoanForm user={user} />
+    </Box>
   )
 }
