971f376a253007255a93c0c62b65453949242f5a
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-07 13:24:57 -0700

    New Loan UX

--- src/components/borrower/CreateLoan/CreateLoanForm.tsx
+++ src/components/borrower/CreateLoan/CreateLoanForm.tsx
@@ -13,6 +13,9 @@ import {
   Container,
   Textarea,
   Select,
+  Text,
+  Flex,
+  Heading,
 } from "@chakra-ui/core"
 import { User, Session } from "../../../utils/types"
 import {
@@ -20,7 +23,7 @@ import {
   Loan_Requests_Insert_Input,
 } from "../../../gql/sdk"
 import { fetcher } from "../../../utils/api"
-import { useRouter } from "next/dist/client/router"
+import { useState } from "react"
 
 type FormData = {
   amount: number
@@ -34,7 +37,7 @@ interface Props {
 
 export default function CreateLoanForm({ user }: Props) {
   const { register, setValue, handleSubmit, errors } = useForm<FormData>()
-  const router = useRouter()
+  const [nSup, supCount] = useState(2)
 
   const onSubmit = (data: FormData) => {
     console.log(data)
@@ -52,12 +55,12 @@ export default function CreateLoanForm({ user }: Props) {
   }
 
   return (
-    <div>
+    <Box>
       <form onSubmit={handleSubmit(onSubmit)} method="post">
         <Container maxW="300px" bg="white">
           <Stack spacing={3}>
             <Input
-              placeholder="Amount in INR"
+              placeholder="Amount (₹)"
               name="amount"
               size="lg"
               ref={register({ required: true })}
@@ -67,22 +70,54 @@ export default function CreateLoanForm({ user }: Props) {
               placeholder="Choose loan purpose"
               ref={register({ required: false })}
             >
-              <option value="School">School</option>
-              <option value="Auto">Auto</option>
+              <option value="School">Educational expense</option>
+              <option value="Auto">Home repair/ renovation</option>
+              <option value="Other">Medical expenses</option>
+              <option value="Other">Wedding in family</option>
+              <option value="Other">Business activity</option>
               <option value="Other">Other</option>
             </Select>
-            <Textarea
-              name="loanSupporters"
-              placeholder="Supporters (name, amount)"
-              ref={register({ required: false })}
-            />
+            <Box h="10px" />
+            <Heading size="md">Supporters</Heading>
+            <Text>Total pledge amount needs to be 20% of loan size</Text>
 
+            {Array.from({ length: nSup }, (x, i) => i).map((idx) => (
+              <Stack key={idx} spacing="10px" minW="280px">
+                <Text>Supporter {idx + 1}: </Text>
+                <Input
+                  placeholder="name"
+                  name="Supporter's name"
+                  size="lg"
+                  ref={register({ required: true })}
+                />
+
+                <Input
+                  placeholder="email address"
+                  name="email"
+                  size="lg"
+                  ref={register({ required: true })}
+                />
+
+                <Input
+                  placeholder="amount"
+                  name="amount"
+                  size="lg"
+                  ref={register({ required: true })}
+                />
+              </Stack>
+            ))}
+            <Center>
+              <Button variant="ghost" onClick={() => supCount(nSup + 1)}>
+                Add Supporter
+              </Button>
+            </Center>
+            <Box h="30px" />
             <Center>
               <Button type="submit">Submit</Button>
             </Center>
           </Stack>
         </Container>
       </form>
-    </div>
+    </Box>
   )
 }
