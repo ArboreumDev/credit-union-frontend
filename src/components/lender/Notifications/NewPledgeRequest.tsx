0d286d1b55b5b3025516934adfcadd413714dc76
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-07 12:00:17 -0700

    Add pledge details

--- src/components/lender/Notifications/NewPledgeRequest.tsx
+++ src/components/lender/Notifications/NewPledgeRequest.tsx
@@ -5,6 +5,7 @@ import {
   AlertTitle,
   Button,
   HStack,
+  Text,
 } from "@chakra-ui/core"
 import { PledgeRequest, CalculatedRisk } from "../../../utils/types"
 import { Currency } from "../../common/Currency"
@@ -24,7 +25,7 @@ export const NewPledgeRequest = ({ pledgeRequest }: Params) => {
       flexDirection="column"
       justifyContent="center"
       textAlign="center"
-      height="280px"
+      height="320px"
       width="inherit"
       marginTop="20px"
       marginBottom="20px"
@@ -35,12 +36,18 @@ export const NewPledgeRequest = ({ pledgeRequest }: Params) => {
       </AlertTitle>
       <AlertDescription>
         {loanRequest.user.name} ({loanRequest.user.email}) is requesting a loan
-        for {loanRequest.purpose}
+        for {loanRequest.purpose} and is asking you to support it.
       </AlertDescription>
       <AlertDescription>
-        Amount <Currency amount={loanRequest.amount} /> | Interest{" "}
-        {riskCalcResult.interestRate}% | Loan Term {riskCalcResult.loanTerm}{" "}
-        months
+        <Text>
+          Total Amount <Currency amount={loanRequest.amount} />
+        </Text>
+        <Text>
+          Your support: <Currency amount={pledgeRequest.pledge_amount} />
+        </Text>
+        <Text>Interest: {riskCalcResult.interestRate}%</Text>
+        <Text>Loan Term: {riskCalcResult.loanTerm} months</Text>
+        <Text>Repayments will be made at the end of every month</Text>
       </AlertDescription>
       <AlertDescription marginTop="20px" maxWidth="sm">
         <HStack>
