d91e8dec3b948a0766b2bbabafd2ef3eee3ce306
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-13 16:46:46 -0700

    Add redirection logic in one function

--- src/utils/ssr.ts
+++ src/utils/ssr.ts
@@ -1,5 +1,30 @@
 import { Session } from "./types"
 import { getSession } from "../../node_modules/next-auth/client"
+import { GetServerSideProps } from "next"
 
-export const getSessionAsProps = async (context) =>
-  (await getSession(context)) as Session
+function getRedirectLocation(page: string, session: Session) {
+  if (page == "/") {
+    if (session && session.user.user_type) return "/dashboard"
+  }
+  if (page == "/dashboard") {
+    if (!session || !session.user || !session.user.user_type) return "/"
+  }
+  if (page == "/onboarding") {
+    if (!session) return "/"
+    if (session.user && session.user.user_type) return "/dashboard"
+  }
+}
+
+const getSSRProps: GetServerSideProps = async ({ req, res }) => {
+  const session = (await getSession({ req })) as Session
+  if (res) {
+    const redirectLocation = getRedirectLocation(req.url, session)
+
+    res.writeHead(301, {
+      Location: redirectLocation,
+    })
+    res.end()
+  }
+
+  return { props: { user: session.user } }
+}
