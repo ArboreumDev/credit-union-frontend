238e5cb5ebe8edf0a68ae88e5be34edda6cd2662
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-13 18:17:50 -0700

    refactor

--- src/components/demo/Main.tsx
+++ src/components/demo/Main.tsx
@@ -1,5 +1,6 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/core"
 import { AddFundsForm } from "pages/dashboard/fund"
+import { UserType } from "utils/types"
 import { DemoTabView } from "../../components/demo/DemoViewTabs"
 import { getDashboardComponent } from "../../pages/dashboard"
 import LoginPage from "../../pages/login"
@@ -15,7 +16,10 @@ export class JStep {
 const bJourneySequence = [
   new JStep("Landing", <LandingPage />),
   new JStep("SignIn", <LoginPage />),
-  new JStep("Onboarding", <Onboarding user={Fixtures.Borrower} />),
+  new JStep(
+    "Onboarding",
+    <Onboarding user={Fixtures.Borrower} userType={UserType.Borrower} />
+  ),
   new JStep("KYC needs approval", getDashboardComponent(Fixtures.Borrower)),
   new JStep(
     "KYC Confirmed",
@@ -36,7 +40,10 @@ const bJourneySequence = [
 const lJourneySequence = [
   new JStep("Landing", <LandingPage />),
   new JStep("SignIn", <LoginPage />),
-  new JStep("Onboarding", <Onboarding user={Fixtures.Lender} />),
+  new JStep(
+    "Onboarding",
+    <Onboarding user={Fixtures.Lender} userType={UserType.Lender} />
+  ),
   new JStep("Lender Dashboard", getDashboardComponent(Fixtures.Lender)),
   new JStep("Add funds", <AddFundsForm user={Fixtures.Lender} />),
   new JStep(
