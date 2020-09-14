e43c40f4af1903237ec8354716b62b72425a97c3
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-13 18:28:48 -0700

    Fix type

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,1 +1,7 @@
-export const irr_dec_to_perc = (irr: number) => (irr * 100).toFixed(1)
+function toFixedNumber(num, digits, base = 10) {
+  const pow = Math.pow(base, digits)
+  return Math.round(num * pow) / pow
+}
+
+export const irr_dec_to_perc = (irr: number, digits = 1) =>
+  toFixedNumber(irr * 100, digits)
