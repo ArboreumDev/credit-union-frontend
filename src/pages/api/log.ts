46e80e8277fe6b0c81f661e9da22affda7859734
Paruthi, Gaurav <gparuthi@umich.edu>
2020-09-22 17:30:14 -0700

    add user id

--- src/pages/api/log.ts
+++ src/pages/api/log.ts
@@ -1,11 +1,16 @@
 import { DbClient } from "gql/db_client"
+import { Session } from "lib/types"
 import { NextApiRequest, NextApiResponse } from "next"
+import { getSession } from "next-auth/client"
 
 export async function LogPushHandler(req: NextApiRequest) {
+  const session = (await getSession({ req })) as Session
+
   const dbClient = new DbClient()
   const { eventType, eventData } = req.body
-
-  return dbClient.logEvent(eventType, eventData, req.headers)
+  const userId =
+    session && session.user && session.user.id ? session.user.id : null
+  return dbClient.logEvent(eventType, eventData, req.headers, userId)
 }
 
 export default async function handler(
