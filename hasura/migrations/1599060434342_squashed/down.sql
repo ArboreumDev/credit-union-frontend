8cc7eda6d08447fc491d9f28e7eb3597784b3b5e
Gaurav Paruthi <gparuthi@umich.edu>
2020-10-27 16:16:36 -0700

    fix down sql

--- hasura/migrations/1599060434342_squashed/down.sql
+++ hasura/migrations/1599060434342_squashed/down.sql
@@ -1,7 +1,7 @@
 DROP TABLE public.edges CASCADE;
 DROP TABLE public.encumbrance_participants CASCADE;
 DROP TABLE public.encumbrances CASCADE;
-DROP TABLE public.guarantors CASCADE;
+DROP TABLE public.supporters CASCADE;
 DROP TABLE public.loan_participants CASCADE;
 DROP TABLE public.loan_requests CASCADE;
 DROP TABLE public.loan_risk CASCADE;
@@ -14,5 +14,6 @@ DROP TABLE public.user CASCADE;
 DROP TYPE public.user_t CASCADE;
 DROP TYPE public.edge_status CASCADE;
 DROP TYPE public.loan_request_status CASCADE;
+DROP TYPE public.supporter_status CASCADE;
 
 DROP FUNCTION public.set_current_timestamp_updated_at CASCADE;
