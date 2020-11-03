01b63acbc1b1b3f3a6fbabf92bd3bb3efa4409e7
Gaurav Paruthi <gparuthi@umich.edu>
2020-11-03 14:23:16 -0800

    make issue

--- src/gql/db_client.ts
+++ src/gql/db_client.ts
@@ -87,14 +87,16 @@ export default class DbClient {
     info?: any
   ) => {
     const user = await this.getUserByEmail(email)
-    if (!user) {
-      const u = await this.sdk.CreateUser({
-        user: {
-          email,
-        },
-      })
-      user.id = u.insert_user_one.id
-    }
+
+    // TODO: #133 Add supporter if it doesn't exists
+    // if (!user) {
+    //   const u = await this.sdk.CreateUser({
+    //     user: {
+    //       email,
+    //     },
+    //   })
+    //   user.id = u.insert_user_one.id
+    // }
     const data = await this.sdk.AddSupporter({
       supporter: {
         request_id: requestId,
