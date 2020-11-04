b857c72e8ead11ea37f42c277ccb9ad10b2319f4
Gaurav Paruthi <gparuthi@umich.edu>
2020-11-03 19:05:48 -0800

    Add supporter

--- tests/src/scenarios/simple.test.ts
+++ tests/src/scenarios/simple.test.ts
@@ -1,18 +1,17 @@
 import * as simple from "../../fixtures/scenarios/simple.json"
 import { ActionType, System, User } from "./types"
 import { uuidv4 } from "./utils"
+import { MIN_SUPPORT_RATIO } from "lib/constant"
 
 const scenario = simple as System
 
 import { dbClient, sdk } from "../common/utils"
 import { Scenario, UserInfo } from "lib/types"
+import { addAndConfirmSupporter } from "../common/test_helpers"
+import { userInfo } from "os"
 
 const users: { [id: string]: User } = {}
 
-interface AdjustBalancePayload {
-  userId: string
-  balanceDelta: number
-}
 async function adjustBalances({ userId, balanceDelta }) {
   const user = users[userId]
   await sdk.ChangeUserCashBalance({
@@ -21,20 +20,28 @@ async function adjustBalances({ userId, balanceDelta }) {
   })
 }
 
-function validateAdjustBalance(
-  before: Scenario,
-  after: Scenario,
-  payload: AdjustBalancePayload
-) {
-  const id = users[payload.userId].id
-  const userBefore = before.users[id] as UserInfo
-  const userAfter = after.users[id] as UserInfo
-  return userBefore.balance + payload.balanceDelta === userAfter.balance
+async function generateOffer({ userId, amount, supporters }) {
+  const user = users[userId]
+  const { request } = await dbClient.createLoanRequest(
+    user.id,
+    amount,
+    "purpose"
+  )
+  // confirm supporter and trigger the loan offer generation
+  for (const sId in supporters) {
+    const supporter = users[sId]
+    await addAndConfirmSupporter(
+      dbClient,
+      request.request_id,
+      supporter.id,
+      amount * MIN_SUPPORT_RATIO
+    )
+  }
 }
 
 export const actionTypeHandlerMap = {
-  // [ActionType.GENERATE_LOAN_OFFER]: (generateOffer),
-  [ActionType.ADJUST_BALANCES]: [adjustBalances, validateAdjustBalance],
+  [ActionType.GENERATE_LOAN_OFFER]: generateOffer,
+  [ActionType.ADJUST_BALANCES]: adjustBalances,
   // [ActionType.CONFIRM_LOAN_OFFER]: confirm_loan_offer,
   // [ActionType.REPAY_LOAN]: repay_loan,
 }
@@ -46,8 +53,6 @@ beforeAll(async () => {
     const user = scenario.state.users[id]
     delete user.encumbered_cash
     delete user.encumbered_portfolio
-    // const uuid = uuidv4()
-    // user.id = uuid
     users[id] = user
     await sdk.CreateUser({ user })
   }
@@ -56,17 +61,20 @@ beforeAll(async () => {
 afterAll(async () => {
   await sdk.ResetDB()
 })
-
+let action
 describe("Adding users and connections", () => {
   test("check scenario", async () => {
     const allUsers = await dbClient.allUsers
     expect(allUsers.length).toBe(Object.keys(scenario.state.users).length)
-    const action = scenario.actions[0]
     const before = await dbClient.getSystemSummary()
-    const [actionMethod, verify] = actionTypeHandlerMap[action.action_type]
-    await actionMethod(action.payload)
-    const after = await dbClient.getSystemSummary()
 
-    expect(verify(before, after, action.payload)).toBe(true)
+    action = scenario.actions[0]
+    await actionTypeHandlerMap[action.action_type](action.payload)
+
+    action = scenario.actions[1]
+    await actionTypeHandlerMap[action.action_type](action.payload)
+
+    const after = await dbClient.getSystemSummary()
+    console.log(after)
   })
 })
