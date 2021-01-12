a9c233984a9b2f3fa1fc9b2f1461b9d0a0d491c5
Gaurav Paruthi <gparuthi@umich.edu>
2021-01-12 11:09:00 -0800

    metadata

--- hasura/migrations/1599060434342_squashed/down.sql
+++ hasura/migrations/1599060434342_squashed/down.sql
@@ -1,17 +1,10 @@
-DROP TABLE public.edges CASCADE;
-DROP TABLE public.encumbrances CASCADE;
 DROP TABLE public.supporters CASCADE;
 DROP TABLE public.loan_participants CASCADE;
 DROP TABLE public.loan_requests CASCADE;
-DROP TABLE public.loan_risk CASCADE;
-DROP TABLE public.payable_type CASCADE;
-DROP TABLE public.payables CASCADE;
-DROP TABLE public.receivables CASCADE;
 DROP TABLE public.recommendation_risk CASCADE;
 DROP TABLE public.user CASCADE;
 
 DROP TYPE public.user_t CASCADE;
-DROP TYPE public.edge_status CASCADE;
 DROP TYPE public.loan_request_status CASCADE;
 DROP TYPE public.supporter_status CASCADE;
 
