b6917fa577a5ac95cb93dc146a80125f905827a2
Gaurav Paruthi <gparuthi@umich.edu>
2021-01-22 19:09:29 -0800

    replace with already existing jstep

--- src/fixtures/Borrower.fixture.tsx
+++ src/fixtures/Borrower.fixture.tsx
@@ -1,27 +1,7 @@
-import BorrowerHome from "components/borrower/BorrowerHome"
-import LandingPage from "components/common/landing"
-import Onboarding from "components/common/onboarding/onboarding"
-import { Fixtures } from "lib/demo/fixtures"
-import { UserType } from "lib/types"
-import LoginPage from "pages/login"
-import React from "react"
+import { bJourneySequence } from "pages/demo/[userType]/[jstep]"
 
-export default {
-  Landing: <LandingPage />,
-  SignIn: <LoginPage />,
-  Onboarding: (
-    <Onboarding user={Fixtures.Borrower} userType={UserType.Borrower} />
-  ),
-  "KYC needs approval": <BorrowerHome user={Fixtures.Borrower} />,
-  "Loan request initiated": (
-    <BorrowerHome user={Fixtures.BorrowerLoanInitiated} />
-  ),
-  "Loan request needs approval": (
-    <BorrowerHome user={Fixtures.BorrowerLoanNeedsConfirmation} />
-  ),
-  "Loan is live": <BorrowerHome user={Fixtures.BorrowerLoanLive} />,
-  Profile: <BorrowerHome user={Fixtures.BorrowerLoanLive} initPanelIdx={1} />,
-  Repayments: (
-    <BorrowerHome user={Fixtures.BorrowerLoanLive} initPanelIdx={2} />
-  ),
-}
+let components = {}
+bJourneySequence.map((j) => {
+  components[j.title] = j.component
+})
+export default components
--- src/fixtures/Lender.fixture.tsx
+++ src/fixtures/Lender.fixture.tsx
@@ -1,21 +1,7 @@
-import LandingPage from "components/common/landing"
-import Onboarding from "components/common/onboarding/onboarding"
-// import { getDashboardComponent } from "pages/dashboard/[[...route]]"
-import LenderHome from "components/lender/LenderHome"
-import { Fixtures } from "lib/demo/fixtures"
-import { UserType } from "lib/types"
-import LoginPage from "pages/login"
-import React from "react"
+import { lJourneySequence } from "pages/demo/[userType]/[jstep]"
 
-export default {
-  Landing: <LandingPage />,
-  SignIn: <LoginPage />,
-  Onboarding: <Onboarding user={Fixtures.Lender} userType={UserType.Lender} />,
-  "Add funds": <LenderHome user={Fixtures.Lender} initPanelIdx={1} />,
-  "With Investment": <LenderHome user={Fixtures.SupporterWithPledge} />,
-  "With No Investment": <LenderHome user={Fixtures.Lender} />,
-  "Supporter with notification": (
-    <LenderHome user={Fixtures.SupporterWithPledgeRequest} />
-  ),
-  Profile: <LenderHome user={Fixtures.Lender} initPanelIdx={2} />,
-}
+let components = {}
+lJourneySequence.map((j) => {
+  components[j.title] = j.component
+})
+export default components
