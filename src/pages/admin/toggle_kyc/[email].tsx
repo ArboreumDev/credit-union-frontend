63778ca7c70192eac3e84a73c1ea921e5a39c852
djudjuu <julfaber@gmail.com>
2021-03-17 19:14:44 +0100

    protect toggle kyc route

--- src/pages/admin/toggle_kyc/[email].tsx
+++ src/pages/admin/toggle_kyc/[email].tsx
@@ -3,14 +3,22 @@ import { getSession } from "next-auth/client"
 import { NextPageContext, GetServerSideProps } from "next"
 import DbClient from "gql/db_client"
 import { Session } from "lib/types"
+import { ADMIN_EMAIL } from "lib/constant"
 
-const Page = (props: { session: Session }) => {
+const Page = (props: { success: boolean }) => {
+  if (!props.success) {
+    return <div>Unauthorized</div>
+  }
   return <div>Toggled kyc</div>
 }
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = (await getSession(context)) as Session
 
   // TODO: Add admin authorization check
+  if (session.user.email !== ADMIN_EMAIL) {
+    return { props: { success: false } }
+  }
+
   const { email } = context.query
 
   const dbClient = new DbClient()
@@ -22,7 +30,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   })
   // TODO send email to user
 
-  return { props: { session } }
+  return { props: { success: true } }
 }
 
 export default Page
