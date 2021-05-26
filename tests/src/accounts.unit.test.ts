d7dc522ea285a6db3d63296635983c58e2c51209
djudjuu <julfaber@gmail.com>
2021-05-26 16:38:23 +0200

    remove old tests / clean up

--- tests/src/accounts.unit.test.ts
+++ tests/src/accounts.unit.test.ts
@@ -33,7 +33,6 @@ afterAll(async () => {
 
 describe("Adding users and connections", () => {
   test("add users", async () => {
-    // add users
     await sdk.CreateUser({ user: LENDER1 })
     await sdk.CreateUser({ user: LENDER2 })
     await sdk.CreateUser({ user: BORROWER1 })
@@ -44,103 +43,30 @@ describe("Adding users and connections", () => {
   })
 
   test("get user by email", async () => {
-    // add users
     const user = await dbClient.getUserByEmail(LENDER1.email)
     expect(user.id === LENDER1.id)
+    expect(user.balance === 0)
   })
 
   describe("Setting and updating user balances", () => {
-    let balancesAfter
-    let balancesBefore
-
     test("changing the balance of one account", async () => {
       await sdk.ChangeUserCashBalance({ userId: LENDER1.id, delta: 42 })
       const allUsers = await dbClient.allUsers
       expect(allUsers.filter((x) => x.id === LENDER1.id)[0].balance).toBe(1042)
     })
-    test("batch updates to multiple accounts", async () => {
-      // moving 41 from lender1 to lender lender2
-      const VALID_UPDATES1 = [
-        {
-          userId: LENDER1.id,
-          balanceDelta: -41,
-          shareDelta: 0,
-          newRoI: NO_ROI,
-        },
-        {
-          userId: LENDER2.id,
-          balanceDelta: 41,
-          shareDelta: 0,
-          newRoI: NO_ROI,
-        },
-      ]
-      await dbClient.updatePortfolios(VALID_UPDATES1)
-
-      const allUsers = await dbClient.allUsers
-      expect(allUsers.filter((x) => x.id === LENDER1.id)[0].balance).toBe(1001)
-      expect(allUsers.filter((x) => x.id === LENDER2.id)[0].balance).toBe(
-        LENDER2.balance + 41
-      )
+    test.todo("batch updates to multiple accounts", async () => {
+      console.log("pass")
     })
 
-    test.skip("batch updates fail if one update is invalid", async () => {
-      const allUsers = await dbClient.allUsers
-      balancesBefore = getUserPortfolio(allUsers)
-
-      // all tx's fail if one update woudl reduce the user balance below 0
-      const INVALID_UPDATES = [
-        {
-          userId: LENDER1.id,
-          balanceDelta: -100000,
-          shareDelta: 0,
-          alias: "lender1",
-          newRoI: NO_ROI,
-        },
-        {
-          userId: LENDER2.id,
-          balanceDelta: 41,
-          shareDelta: 0,
-          alias: "lender2",
-          newRoI: NO_ROI,
-        },
-      ]
-
-      const result = await dbClient.updatePortfolios(INVALID_UPDATES)
-      expect(result).toHaveProperty("ERROR")
-
-      // all balances should be the same as before
-      const balancesAfter = getUserPortfolio(allUsers)
-      expect(balancesBefore).toStrictEqual(balancesAfter)
+    test.todo("batch updates fail if one update is invalid", async () => {
+      console.log("pass")
     })
 
-    test("multiple updates to the same account are taken together", async () => {
-      let allUsers = await dbClient.allUsers
-      balancesBefore = getUserPortfolio(allUsers)
-      const VALID_UPDATES2 = [
-        {
-          userId: LENDER1.id,
-          balanceDelta: -100010,
-          shareDelta: 0,
-          alias: "firstTx",
-          newRoI: NO_ROI,
-        },
-        {
-          userId: LENDER1.id,
-          balanceDelta: +100000,
-          shareDelta: 0,
-          alias: "secondTx",
-          newRoI: NO_ROI,
-        },
-      ]
-      // even though the first updates reduces the user balance below 0, the tx go trough
-      // because the second add enough to be net positive
-      await dbClient.updatePortfolios(VALID_UPDATES2)
-
-      allUsers = await dbClient.allUsers
-      balancesAfter = getUserPortfolio(allUsers)
-      expect(balancesAfter[LENDER1.id].cash).toBe(
-        balancesBefore[LENDER1.id].cash - 10
-      )
-    })
+    test.todo(
+      "multiple updates to the same account are taken together",
+      async () => {
+        console.log("pass")
+      }
+    )
   })
 })
--- tests/src/common/test_helpers.ts
+++ tests/src/common/test_helpers.ts
@@ -4,28 +4,7 @@ import { SupporterStatus } from "../../../src/lib/types"
 export const getUserPortfolio = (userList) => {
   const ret = {}
   userList.forEach((user) => {
-    ret[user.id] = { cash: user.balance, share: user.corpus_share }
+    ret[user.id] = { cash: user.balance }
   })
   return ret
-}
-
-export const addAndConfirmSupporter = async (
-  dbClient: DbClient,
-  request_id: string,
-  supporter_id: string,
-  pledge_amount: number
-) => {
-  await dbClient.sdk.AddSupporter({
-    supporter: {
-      request_id,
-      supporter_id,
-      pledge_amount,
-    },
-  })
-  await dbClient.updateSupporter(
-    request_id,
-    supporter_id,
-    SupporterStatus.confirmed,
-    pledge_amount
-  )
 }
